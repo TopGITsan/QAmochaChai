e1d07270920145f797f9da73667b4ac414799464
TopGitSan <c.toparcean@gmail.com>
2019-04-29 12:46:16 +0200

    async assert test endpoint /hello

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -45,7 +45,7 @@ suite('Functional Tests', function() {
       
       /** Ready to have a try ?
        * Replace assert.fail(). Make the test pass. **/
-       
+
       // If no name is passed, the endpoint responds with 'hello Guest'.
       test('Test GET /hello with no name',  function(done){ // Don't forget the callback...
          chai.request(server)             // 'server' is the Express App
@@ -55,8 +55,8 @@ suite('Functional Tests', function() {
             // Test the status and the text response (see the example above). 
             // Please follow the order -status, -text. We rely on that in our tests.
             // It should respond 'Hello Guest'
-            assert.fail(res.status, 200);
-            assert.fail(res.text, 'hello Guest');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'hello Guest');
             done();   // Always call the 'done()' callback when finished.
           });
       });
@@ -64,14 +64,14 @@ suite('Functional Tests', function() {
       /**  Another one... **/
       test('Test GET /hello with your name',  function(done){ // Don't forget the callback...
          chai.request(server)             // 'server' is the Express App
-          .get('/hello?name=xy_z') /** <=== Put your name in the query **/ 
+          .get('/hello?name=Top') /** <=== Put your name in the query **/ 
           .end(function(err, res){        // res is the response object
           
             // Your tests here.
             // Replace assert.fail(). Make the test pass.
             // Test the status and the text response. Follow the test order like above.
-            assert.fail(res.status, 200);
-             assert.fail(res.text, 'hello xy_z'/** <==  Put your name here **/);
+            assert.equal(res.status, 200);
+             assert.equal(res.text, 'hello Top'/** <==  Put your name here **/);
             done();   // Always call the 'done()' callback when finished.
           });
       });
@@ -89,7 +89,7 @@ suite('Functional Tests', function() {
     // ### EXAMPLE ### 
     suite('PUT /travellers', function(){
       test('#example - responds with appropriate JSON data when sending {surname: "Polo"}',  function(done){
-         chai.request(server)
+          chai.request(server)
           .put('/travellers')         // note the PUT method
           .send({surname: 'Polo'})    // attach the payload, encoded as JSON
           .end(function(err, res){    // Send the request. Pass a Node callback
